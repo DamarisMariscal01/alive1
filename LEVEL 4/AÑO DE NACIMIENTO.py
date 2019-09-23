77f252cc6045d6cabbe002e3f04146496df2143c
Damaris <damarisrm@gmail.com>
2019-09-23 17:17:35 -0500

    Clase 2

---
+++ LEVEL 4/AÑO DE NACIMIENTO.py
@@ -0,0 +1,7 @@
+#Input
+año = int(input ("Dime tu año de nacimiento: "))
+
+#Proceso
+edad = 2019 - año
+
+print ("Esta es la edad aproximada que tienes: " , edad)
---
+++ LEVEL 4/DESCUENTO.py
@@ -0,0 +1,7 @@
+#Input
+compra = int(input ("Dime el precio total de tu compra: "))
+
+#Proceso
+descuento = compra - (compra * 0.15)
+
+print ("Este es el total a pagar con el descuento incluido: " , descuento)
